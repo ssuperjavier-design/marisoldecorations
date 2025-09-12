2676a21e1d8932a65b74471b84826b1729def88b
ssuperjavier-design <ssuperjavier@gmail.com>
2025-09-12 11:18:26 -0700

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
       <div 
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: url("https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/509576860_9945499838860894_483000244043863548_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=5AJCdfrYv1MQ7kNvwHSbv_n&_nc_oc=AdnSxD6jnc55QL8VGHxS3RQy6O7BuCMwSDQKK5iE-qzF67H7l75nUX8AVbNdcj1F4_LqC1o0zs4lSuKFCCjE6qtS&_nc_zt=23&_nc_ht=scontent-sjc3-1.xx&_nc_gid=Pm4iq-SM1VsK2Bque8eVWg&oh=00_AfaoEfoty-E8mKbXFYuzpsLs7ZrrxE_uZpqpAeCOjgOi8w&oe=68C289D3")`
+          backgroundImage: 'url("https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/509576860_9945499838860894_483000244043863548_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=5AJCdfrYv1MQ7kNvwHSbv_n&_nc_oc=AdnSxD6jnc55QL8VGHxS3RQy6O7BuCMwSDQKK5iE-qzF67H7l75nUX8AVbNdcj1F4_LqC1o0zs4lSuKFCCjE6qtS&_nc_zt=23&_nc_ht=scontent-sjc3-1.xx&_nc_gid=Pm4iq-SM1VsK2Bque8eVWg&oh=00_AfaoEfoty-E8mKbXFYuzpsLs7ZrrxE_uZpqpAeCOjgOi8w&oe=68C289D3")`
         }}
       >
         <div className="absolute inset-0 bg-gradient-to-br from-pink-900/70 via-black/50 to-pink-900/70"></div>
